def1b048d5a0ea4b26da00db55daee9a6f70cca9
Ariel Altamirano <leandro.a.altamirano@gmail.com>
2016-09-29 10:45:48 -0300

    Data is not neede for the control to work.

--- examples/KendouiExtendedApi/Scripts/Partials/KendoUiExt.SlideUpList.js
+++ examples/KendouiExtendedApi/Scripts/Partials/KendoUiExt.SlideUpList.js
@@ -3,8 +3,8 @@
 KendoUiExt.SlideUpList = function () {
 	var load = function () {
 	    $("#targetDiv").kendoExtSlideUpList({
-	        dataSource: ["Item 1", "Item 2", "Item 3"]
-	    }).data("kendoExtSlideUpList");
+	        dataSource: ["Item 1", "Item 2", "Item 3", "Item 4"]
+	    });//.data("kendoExtSlideUpList");
 	};
 
 	var unload = function () {
